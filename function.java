2014fcfbdc671c26611387c10b52b39882aa877b
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-08 23:40:29 +0500

    start learning function

---
+++ function.java
@@ -0,0 +1,9 @@
+public class function{
+    public static void helloprint(){
+System.out.println("hello world");
+    }
+
+    public static void main(String[] args){
+        helloprint();
+    }
+}
