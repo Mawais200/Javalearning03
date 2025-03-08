7972218297951c835a1c11000f1a25c38c4d3e9b
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-08 23:16:59 +0500

    check if a number is prime or not

--- loop.java
+++ loop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class loop {
     public static void main(String[] args) {
         // run while loop to print  1 to 10 digits
@@ -14,7 +16,7 @@ public class loop {
         System.out.print(lastdigit);
         number=number/10;
        }
-       // Break and Continues Statment
+       // Break  Statment
        for(int i=1; i<=10; i++){
         if(i==4){
             System.out.println(i);
@@ -22,5 +24,38 @@ public class loop {
         }
         System.out.println("i am out of loop ");
        }
+       // Continues Statment
+       for(int i=1; i<=10; i++){
+        if(i==4){
+            System.out.println(i);
+            continue;
+        }
+        System.out.println("i am out of loop ");
+       }
+       //check if a number is prime or not
+
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter a number to check wheather it is prime or not");
+       int n = sc.nextInt();
+       if (n ==2){
+        System.out.println("the enter number is prime");
+       }else{
+        boolean isPrime =true;
+       for (int i =2; i<n-1;i++){
+        if(n%i==0){
+            isPrime=false;
+        }
+
+       }
+       if(isPrime== true){
+        System.out.println("this number is prime");
+
+       }
+       else{
+        System.out.println("this number is not a prime");
+       }
+       }
+       
     }
+
 }
