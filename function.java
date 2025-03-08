656f924b982f2ad4313abccde5099c2eb3b8bfe5
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-08 23:58:29 +0500

    function with parameter to calculate sum of a and b

--- function.java
+++ function.java
@@ -1,9 +1,21 @@
-public class function{
-    public static void helloprint(){
-System.out.println("hello world");
-    }
+import java.util.Scanner;
 
+public class function{
+    public static int Sum(int a,int b){
+        int sum=a+b;
+       return sum;
+        
+
+    }
     public static void main(String[] args){
-        helloprint();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b:");
+        int b = sc.nextInt();
+        int sum = Sum(a, b);
+        System.out.println("sum of a and b is:" +sum);
+
+        
     }
 }
